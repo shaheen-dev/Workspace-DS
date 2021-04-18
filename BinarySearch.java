0176ba3a3c9d6f033b0982c73c1e362af77b6781
shaheen-dev <72076536+shaheen-dev@users.noreply.github.com>
2021-04-18 16:03:49 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class BinarySearch
+{
+    int binarySearch(int arr[],int s,int l,int r)
+    {
+        if(l<=r)
+        {
+            int mid = (l+r)/2;
+            if(arr[mid]==s)
+            {
+                return mid;
+            }
+            else if(arr[mid]<s)
+              return binarySearch( arr, s, mid+1, r);
+            else
+                return binarySearch( arr, s, l, mid-1);
+            
+        }
+        else
+            return -1;
+    }
+	public static void main(String[] args) {
+	    Main m=new Main();
+	   
+	    
+	    int arr[] = {10,20,30,40,50,60,70};
+	    int l=0,r=arr.length-1;
+	    Scanner sc=new Scanner(System.in);
+	    int s=sc.nextInt();
+	    
+		int result = m.binarySearch(arr,s,l,r);
+		if(result == -1)
+		    System.out.println("Not found ");
+		else
+		     System.out.println(s+" found at index  "+result);
+	}
+}
